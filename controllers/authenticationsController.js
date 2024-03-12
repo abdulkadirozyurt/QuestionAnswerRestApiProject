d0cbc8543a52da36d8bc37cb55b1ba52e0ec7b50
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2024-03-12 21:53:17 +0300

    first record added with register function post request)

--- controllers/authenticationsController.js
+++ controllers/authenticationsController.js
@@ -1,8 +1,25 @@
-const register = (req, res, next) => {
+import UserModel from "../models/user.js";
+
+const register = async (req, res, next) => {
+  const name = "Abdulkadir Özyurt";
+  const email = "abdulkadir.ozyurt@gmail.com";
+  const password = "123456";
+
+  //async await
+  const user = await UserModel.create({
+    // name: name,
+    // email: email,
+    // password: password,
+
+    name,
+    email,
+    password,
+  });
+
   res.status(200).json({
     success: true,
+    data: user,
   });
 };
 
-
-export{register}
+export { register };
--- routers/authenticationRouter.js
+++ routers/authenticationRouter.js
@@ -4,6 +4,7 @@ import { register } from "../controllers/authenticationsController.js";
 // api/authentications
 // bu express.Router aslında bir middleware
 const authenticationRouter = express.Router();
+
 authenticationRouter.post("/register", register);
 
 export { authenticationRouter };
